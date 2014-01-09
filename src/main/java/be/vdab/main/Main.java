fb58d4342863a6110e85b507229a2b1cd42d5aef
olivier <olivier.deleye@gmail.com>
2014-01-09 16:01:46 +0100

    version 1.1 09/01/2014

--- src/main/java/be/vdab/main/Main.java
+++ src/main/java/be/vdab/main/Main.java
@@ -10,12 +10,12 @@ public class Main {
 
 	public static void main(String[] args) {
 	   try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/restClient.xml")) {
-			FiliaalClient filiaalClient = context.getBean(FiliaalClient.class);
-			FiliaalListREST filiaalListREST = filiaalClient.findAll();
-			for (FiliaalListItemREST filiaalItem : filiaalListREST.getFilialen()) {
-			System.out.println(filiaalItem.getId() + ":" + filiaalItem.getNaam());
+			  FiliaalClient filiaalClient = context.getBean(FiliaalClient.class);
+			  FiliaalListREST filiaalListREST = filiaalClient.findAll();
+			  for (FiliaalListItemREST filiaalItem : filiaalListREST.getFilialen()) {
+			  System.out.println(filiaalItem.getId() + ":" + filiaalItem.getNaam());
 			}
-			}
+		}
 
 	}
 
